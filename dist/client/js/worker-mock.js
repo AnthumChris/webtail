b8f0eeaf9a13a24c8dd572292f828cd3338eace6
Chris McGowan <chris.mcgowan@ibm.com>
2019-02-26 22:12:27 -0500

    added threat color for mock data

--- dist/client/js/worker-mock.js
+++ dist/client/js/worker-mock.js
@@ -43,7 +43,7 @@ function getRandomLine({ showError, cpuSystem, cpuProcess, memTotal, memFree}) {
 	cpuSystem = cpuSystem.toFixed(2);
 	cpuProcess = cpuProcess.toFixed(2);
 
-	let s = `<div><span class="time">${time}</span> [pool-${pool}-thread-1] ${level} <span class="class">c.c.u.monitoring.Co3SystemAnalyzer</span>`;
+	let s = `<div><span class="time">${time}</span> <span class="thread">[pool-${pool}-thread-1]</span> ${level} <span class="class">c.c.u.monitoring.Co3SystemAnalyzer</span>`;
 
 	if (!showError) {
 		s += ` - <span class="label">Perf Info</span> - OSName: Mac OS X, OSVersion: 10.14.3, SystemCPU: ${cpuSystem}, ProcessCPU: ${cpuProcess}, NumCPUs: 8, MemPhysMB: ${memTotal}, MemFreeMB: ${memFree}, MemPctFree: 5.96, SwapTotalMB: 3072, SwapFreeMB: 1365, SwapPctFree: 44.43, JVMUptimeSec: 116080, JVMHeapInitMB: 256, JVMHeapUsedMB: 661, JVMHeapCommitMB: 1002, JVMHeapMaxMB: 3641, JVMHeapUsedCommitPct: 65.97, JVMHeapCommitMaxPct: 27.52, JVMNonHeapInitMB: 2, JVMNonHeapUsedMB: 358, JVMNonHeapCommitMB: 374, JVMNonHeapMaxMB: 0, YoungNumGC: 274, YoungTimeGCms: 4918, YoungTimePerGCms: 17, YoungPctUptimeGC: 0.00, OldNumGC: 6, OldTimeGCms: 2120, OldTimePerGCms: 353, OldPctUptimeGC: 0.00, NetTimeGCms: 7038, NetPctUptimeGC: 0.01, PctTimeLastMinGC: 0.00</div>`;
