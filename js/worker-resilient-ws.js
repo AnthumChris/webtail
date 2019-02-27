da280b1aed784bf35588a7f3e656741a0bbcd795
Chris McGowan <chris.mcgowan@ibm.com>
2019-02-27 17:02:44 -0500

    convert tabs to spaces

--- js/worker-resilient-ws.js
+++ js/worker-resilient-ws.js
@@ -1,18 +1,18 @@
 // match monitoring.log patterns to parse numbers
 const chartValPatterns = new RegExp([
-	'SystemCPU: (\\d+(\\.\\d+)?)',  // 1
-	'ProcessCPU: (\\d+(\\.\\d+)?)', // 3
-	'MemPhysMB: (\\d+)',            // 5
-	'MemFreeMB: (\\d+)',            // 6
+  'SystemCPU: (\\d+(\\.\\d+)?)',  // 1
+  'ProcessCPU: (\\d+(\\.\\d+)?)', // 3
+  'MemPhysMB: (\\d+)',            // 5
+  'MemFreeMB: (\\d+)',            // 6
 ].join('.*'));
 
 // regex pattern replacements
 const perfLogReplacements = [
-	// XSS
-	[/&/g, '&amp;'],
-	[/</g, '&lt;'],
+  // XSS
+  [/&/g, '&amp;'],
+  [/</g, '&lt;'],
 
-	// visual decorators
+  // visual decorators
   [/(\d\d:\d\d:\d\d\.\d\d\d)/, '<span class="time">$1</span>'],
   [/(\[pool.*\])/, '<span class="thread">$1</span>'],
   [/ (DEBUG) /, ' <span class="level-debug">$1</span> '],
@@ -25,53 +25,53 @@ const perfLogReplacements = [
 const socket = new WebSocket('ws://localhost:4000/');
 socket.onopen = _ => console.log('WS opened!', this);
 socket.onerror = function(e) {
-	if (socket.readyState === 3) {
-		return sendError('Could not open web socket connection.');
-	}
+  if (socket.readyState === 3) {
+    return sendError('Could not open web socket connection.');
+  }
 }
 socket.onmessage = e => processSocketData(e.data);
 
 function processSocketData(data) {
-	try {
-		data = JSON.parse(data);
-	} catch (e) {
-		return sendError('Could not parse JSON response');
-	}
+  try {
+    data = JSON.parse(data);
+  } catch (e) {
+    return sendError('Could not parse JSON response');
+  }
 
-	if (data.error) {
-		return sendError(data.error);
-	}
+  if (data.error) {
+    return sendError(data.error);
+  }
 
-	// re-format respons, parse chart data, and post to app
-	const postData = {};
+  // re-format respons, parse chart data, and post to app
+  const postData = {};
 
-	if (data.fileName)
-		postData.fileName = data.fileName;
+  if (data.fileName)
+    postData.fileName = data.fileName;
 
-	if (data.line) {
-		// parse last line for chart values and add to postData
-		const chartValMatches = data.line.match(chartValPatterns);
-		if (chartValMatches && chartValMatches.length > 6) {
-			const memTotal = parseInt(chartValMatches[5]);
-			const memFree = parseInt(chartValMatches[6]);
+  if (data.line) {
+    // parse last line for chart values and add to postData
+    const chartValMatches = data.line.match(chartValPatterns);
+    if (chartValMatches && chartValMatches.length > 6) {
+      const memTotal = parseInt(chartValMatches[5]);
+      const memFree = parseInt(chartValMatches[6]);
 
-			postData.chart = {
-				cpuSystem: parseFloat(chartValMatches[1]),
-				cpuProcess: parseFloat(chartValMatches[3]),
-				memUsed: memTotal-memFree, 
-				memFree: memFree
-			}
-		}
+      postData.chart = {
+        cpuSystem: parseFloat(chartValMatches[1]),
+        cpuProcess: parseFloat(chartValMatches[3]),
+        memUsed: memTotal-memFree, 
+        memFree: memFree
+      }
+    }
 
-		const line = perfLogReplacements.reduce((val, params) => val.replace.apply(val, params), data.line);
-		postData.lines = [`<div>${line}</div>`];
-	}
+    const line = perfLogReplacements.reduce((val, params) => val.replace.apply(val, params), data.line);
+    postData.lines = [`<div>${line}</div>`];
+  }
 
-	self.postMessage(postData);
+  self.postMessage(postData);
 }
 
 function sendError(errorMessage) {
-	self.postMessage({
-		error: 'Error: '+errorMessage
-	})
+  self.postMessage({
+    error: 'Error: '+errorMessage
+  })
 }
